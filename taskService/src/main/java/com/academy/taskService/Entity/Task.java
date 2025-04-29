37b3d000d524d011a5d7f0bd8d471262a7026e24
Vladislav Ostanin <ostanin8900@yandex.ru>
2025-04-29 15:20:30 +0500

    add entity

---
+++ taskService/src/main/java/com/academy/taskService/Entity/Task.java
@@ -0,0 +1,39 @@
+package com.academy.taskService.Entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    // Пока оставлю Long'ом
+    private Long userId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
